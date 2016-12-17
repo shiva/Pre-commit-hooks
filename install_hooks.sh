28a3123edaae85d040fb39f460347b8fc2228334
Shiva <shiv@shiv.me>
2016-12-17 14:38:14 -0800

    Add more hooks
     - Add clang-format and disallow-non-text hooks
     - update install hooks

--- install_hooks.sh
+++ install_hooks.sh
@@ -13,7 +13,7 @@
 # CONFIGURATION:
 # select which pre-commit hooks are going to be installed
 #HOOKS="pre-commit pre-commit-compile pre-commit-uncrustify"
-HOOKS="pre-commit pre-commit-compile pre-commit-uncrustify"
+HOOKS="pre-commit pre-commit-trailing-whitespace pre-commit-clang-format pre-commit-compile pre-commit-uncrustify"
 ###########################################################
 # There should be no need to change anything below this line.
 
